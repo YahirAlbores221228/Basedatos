71119e559c5865531ca2c31a6866a89c3823ec14
YahirAlbores221228 <programcion2023@gmail.com>
2023-03-24 19:53:38 -0600

    nuevo metodo SNS

--- Api/controllers/usuario.js
+++ Api/controllers/usuario.js
@@ -1,4 +1,14 @@
 const Usuario = require("../model/usuario");
+const AWS = require('aws-sdk');
+
+//mandada de mensajaes
+const sns = new AWS.SNS({
+  profile: 'receita',
+  accessKeyId: 'AKIA4IDAIOVAA3XBTFXY',
+  secretAccessKey: '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',
+  region: 'us-east-1',
+});   
+
 // Obtener todos los objetos de usuario
 const getUsuarios = async (req, res) => {
   Usuario.find((err, usuarios) => {
@@ -26,6 +36,7 @@ const validLogin = async (req, res) => {
         console.log('if Password')
         datos.push(user._id, user.Nombre)
         console.log(username, password, datos)
+
         return res.status(200).send({ message: "Bienvenido" })
       } else {
         return res.status(400).send({ message: "Contraseña incorrecta" })
@@ -52,10 +63,23 @@ console.log(req.body); // Verificar el valor de req.body
     Contrasena: req.body.Contrasena,
     Correo: req.body.Correo,
   });
-usuario.save( async (err, usuario) => {
+  usuario.save( async (err, usuario) => {
     if (err) {
       res.send(err);
     }
+//envia los gmail
+ let now = new Date().toString();
+    let email = `Nuevo usuario registrado \n \n Enviado: ${now}`;
+    let params = {
+        Message: email,
+        TopicArn: 'arn:aws:sns:us-east-1:842015470912:receita'
+    };
+
+    sns.publish(params, function(err, data) {
+      if (err) console.log(err, err.stack); 
+      else console.log(data);
+    });
+
     res.json(usuario);
   });
 };
